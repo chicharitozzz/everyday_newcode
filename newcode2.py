56a2bcd6ef267fac3777bc8ebf3fe5c26352a57d
chicharito <445027386@qq.com>
2018-06-19 15:25:30 +0800

    6.19

--- newcode2.py
+++ newcode2.py
@@ -14,3 +14,31 @@ class Solution:
                 self.stack2.append(self.stack1.pop())
 
         return sel.stack2.pop()
+
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+    def Merge(self, pHead1, pHead2):
+        # write code here
+        if not pHead1:
+            return pHead2
+        if not pHead2:
+            return pHead1
+
+        merge_list = None
+        if pHead1.val < pHead2.val:
+            merge_list = pHead1
+            merge_list.next = self.Merge(pHead1.next, pHead2)
+
+        else:
+            merge_list = pHead2
+            merge_list.next = self.Merge(pHead1, pHead2.next)
+
+        return merge_list
+
+
+if __name__ == '__main__':
+    s = Solution()
+    r = s.Merge([], [])
+    print(r)
